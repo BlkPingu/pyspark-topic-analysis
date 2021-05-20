95c396ca55f059591783d2ac988b89b1af9642f4
Roman <s0547499@htw-berlin.de>
2021-05-20 22:43:53 +0200

    better spacy performance, pickling, etc.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-import os
 import glob
 import spacy
 from pyspark import SparkContext
@@ -9,48 +8,61 @@ from pyspark.sql import Row
 from pyspark.ml.feature import CountVectorizer
 from pyspark.ml.feature import IDF
 from pyspark.ml.clustering import LDA
+import pickle
+import os
 
-nlp = spacy.load('en_core_web_sm')
-nlp.max_length = 9999999
-nlp.add_pipe('merge_noun_chunks')
+if __name__ == '__main__':
+    if not os.path.exists('documents.p'):
+        # Import and preprocess documents
+        raw_documents = []
+        for file in glob.glob("texts/*.txt"):
+            with open(file, 'r', encoding='utf-8') as f:
+                raw_documents.append(f.read().replace('\n', ' '))
 
-os.environ["HADOOP_HOME"] = r'C:\Users\Roman\Desktop\sparkproto\spark'  # is that even needed?
+        nlp = spacy.load('en_core_web_sm', exclude=['ner'])
+        nlp.max_length = 9999999
 
-# Import and preprocess documents
-documents = []
-for file in glob.glob("texts/*.txt"):
-    with open(file, 'r', encoding='utf-8') as f:
-        print(file)
-        text = f.read().replace('\n', ' ')
-        doc = nlp(text)
-        documents.append([token.lemma_.lower() for token in doc if token.pos_ in ['NOUN', 'VERB']])
+        documents = []
+        for doc in nlp.pipe(raw_documents, n_process=-1, batch_size=1):
+            documents.append([token.lemma_.lower() for token in doc
+                              if token.pos_ == 'NOUN'
+                              and token.is_alpha
+                              and not token.is_stop
+                              and len(token.lemma_) > 3])
 
-spark_context = SparkContext(appName="TF-IDF & LDA Prototype")
-spark_session = SparkSession(spark_context)
+        with open('documents.p', 'wb') as f:
+            pickle.dump(documents, f)
+    else:
+        with open('documents.p', 'rb') as f:
+            documents = pickle.load(f)
 
-# Create DataFrame
-rdd1 = spark_context.parallelize(documents)
-row_rdd = rdd1.map(lambda x: Row(x))
-documents_df = spark_session.createDataFrame(row_rdd, ['words'])
+    spark_context = SparkContext(appName="TF-IDF & LDA Prototype")
+    spark_session = SparkSession(spark_context)
 
-# https://github.com/maobedkova/TopicModelling_PySpark_SparkNLP/blob/master/Topic_Modelling_with_PySpark_and_Spark_NLP.ipynb
+    # Create DataFrame
+    rdd1 = spark_context.parallelize(documents)
+    row_rdd = rdd1.map(lambda x: Row(x))
+    documents_df = spark_session.createDataFrame(row_rdd, ['words'])
 
-# Calculate tf
-tf = CountVectorizer(inputCol='words', outputCol='tf_features')
-tf_model = tf.fit(documents_df)
-tf_result = tf_model.transform(documents_df)
+    # https://github.com/maobedkova/TopicModelling_PySpark_SparkNLP/blob/master/Topic_Modelling_with_PySpark_and_Spark_NLP.ipynb
 
-# Calculate tf-idf
-idf = IDF(inputCol='tf_features', outputCol='tf_idf_features')
-idf_model = idf.fit(tf_result)
-tfidf_result = idf_model.transform(tf_result)
+    # Calculate tf
+    tf = CountVectorizer(inputCol='words', outputCol='tf_features')
+    tf_model = tf.fit(documents_df)
+    tf_result = tf_model.transform(documents_df)
 
-# Calculate topics
-lda = LDA(k=6, maxIter=10, featuresCol='tf_idf_features')
-lda_model = lda.fit(tfidf_result)
+    # Calculate tf-idf
+    idf = IDF(inputCol='tf_features', outputCol='tf_idf_features', minDocFreq=2)
+    idf_model = idf.fit(tf_result)
+    tfidf_result = idf_model.transform(tf_result)
 
-udf_to_words = functions.udf(lambda token_list: [tf_model.vocabulary[token_id] for token_id in token_list],
-                             types.ArrayType(types.StringType()))
+    # Calculate topics
+    lda = LDA(k=20, maxIter=10, featuresCol='tf_idf_features')
+    lda_model = lda.fit(tfidf_result)
 
-topics = lda_model.describeTopics(10).withColumn('topicWords', udf_to_words(functions.col('termIndices')))
-topics.select('topic', 'topicWords').show(truncate=90)
+    vocab = tf_model.vocabulary
+    udf_to_words = functions.udf(lambda token_list: [vocab[token_id] for token_id in token_list],
+                                 types.ArrayType(types.StringType()))
+
+    topics = lda_model.describeTopics(10).withColumn('topicWords', udf_to_words(functions.col('termIndices')))
+    topics.select('topic', 'topicWords').show(truncate=200)
